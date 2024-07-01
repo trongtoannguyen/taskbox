84189651111d24f6fc5a175e38a8b41ef5a93cb2
trongtoannguyen <trongtoan1609ht@gmail.com>
2024-07-02 01:27:05 +0700

    Deploy Storybook cli: yarn build-storybook

---
+++ storybook-static/assets/Header.stories-NxIpwkU5.js
@@ -0,0 +1,7 @@
+import{H as p}from"./Header-CE4Yflpo.js";import{f as o}from"./index-xheb6HNg.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./Button-bugnTYXA.js";import"./index-jd2klTXJ.js";const x={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,m,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const O=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,O as __namedExportsOrder,x as default};
