63317b4365bdce7b34a52b0edb7ce647b9e170eb
trongtoannguyen <trongtoan1609ht@gmail.com>
2024-07-02 01:12:36 +0700

    feat: Configure MSW for mocking API requests
    This commit adds the necessary configuration to use MSW for mocking API requests in the application. The `package.json` file is updated to include the `"msw"` property with the `"workerDirectory"` set to `"public"`. Additionally, the `.storybook/preview.js` file is modified to import and initialize the MSW addon.
    
    These changes enable the use of MSW for mocking API responses during development and testing, improving the efficiency of frontend development workflows.

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -1,6 +1,7 @@
 import InboxScreen from "./InboxScreen";
 import store from "../lib/store";
-
+import { http, HttpResponse } from "msw";
+import { MockedState } from "./TaskList.stories";
 import { Provider } from "react-redux";
 
 export default {
@@ -10,6 +11,28 @@ export default {
   tags: ["autodocs"],
 };
 
-export const Default = {};
+export const Default = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://jsonplaceholder.typicode.com/todos?userId=1", () => {
+          return HttpResponse.json(MockedState.tasks);
+        }),
+      ],
+    },
+  },
+};
 
-export const Error = {};
+export const Error = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://jsonplaceholder.typicode.com/todos?userId=1", () => {
+          return new HttpResponse(null, {
+            status: 403,
+          });
+        }),
+      ],
+    },
+  },
+};
